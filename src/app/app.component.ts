58e03e6e20f52dae5ba6287b970c145a55c45144
Prathuish Gera <gprathuis@gmail.com>
2019-05-21 22:58:11 +0530

    ngIf demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-directives-and-pipes';
+  title = '*ngIf - Demo';
+
+  myCustomArray: Array<number> = [1, 2, 3];
+
 }
